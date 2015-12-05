e44b5e31fc1a58877057dcafd4a86a4197d2ae80
Todd Dukart <tdukart@aloftinteractive.com>
2015-12-05 11:03:24 -0500

    Don't redirect errors

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git config user.email "tdukart@aloftinteractive.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
